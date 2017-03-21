de087d179ebf42789fe95d100f6c0f021a19302f
weders <weders@student.ethz.ch>
2017-03-21 16:28:46 +0100

    hard baseline check!

--- task0/predict.py
+++ task0/predict.py
@@ -3,6 +3,10 @@ import sklearn
 from numpy import genfromtxt
 from sklearn.metrics import mean_squared_error
 from sklearn import linear_model
+import csv
+
+from math import fsum
+import pandas
 
 ######################################################
 # Preferences
@@ -32,7 +36,7 @@ ridgeregression = True
 ######################################################
 
 
-#load data from csv file to np.array 
+#load data from csv file to np.array
 print("loading data...")
 train_data = np.genfromtxt ('train.csv', delimiter=",")
 test_data = np.genfromtxt ('test.csv', delimiter=",")
@@ -41,199 +45,23 @@ test_data = np.array(test_data)
 #test_data
 x_test = test_data[1:2001,1:11]
 #train and validate data
-x_train = train_data[1:4999,2:12]
-y_train = train_data[1:4999,1]
-x_validate = train_data[5000:10000,2:12]
-y_validate = train_data[5000:10000,1]
+x_train = train_data[1:9000,2:12]
+y_train = train_data[1:9000,1]
+x_validate = train_data[9001:10000,2:12]
+y_validate = train_data[9001:10000,1]
 
+y_test = []
 
-######################################################
-# Least Squares Implementation
-######################################################
+for i in x_test:
+    y_test.append(fsum(i)/i.shape[0])
 
-if leastsquares:
+y_test = np.asarray(y_test)
+test_Id = test_data[1:,0]
+test_Id = np.reshape(test_Id,(test_Id.shape[0],1))
+y_test = np.reshape(y_test,test_Id.shape)
 
-    # compute variance and covariance
-    print("computing variance matrix and covariance matrix...")
+output = np.hstack((test_Id,y_test))
 
-
-    x_train_tra = x_train.transpose()
-    var = x_train_tra.dot(x_train)
-    covar = x_train_tra.dot(y_train)
-
-    # compute weigths
-    print("computing weights...")
-    var_inv = np.linalg.inv(var)
-    weights = var_inv.dot(covar)
-    print(weights)
-    # predict validate data
-    print("calculating predictions...")
-    prediction = x_validate.dot(weights)
-    prediction = prediction.transpose()
-    # rmse
-    print("calculate RMSE..")
-    RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-    print("RMSE:{0:0.15f}".format(RMSE))
-
-
-
-######################################################
-# Gradient Descent Implementation
-######################################################
-
-if gradientdescent:
-    mean = np.mean(x_train,axis=0)
-    mean = np.reshape(mean,(10,1))
-
-    variance = (x_train.T.dot(x_train))
-
-    eig_val,eig_vec = np.linalg.eigh(variance)
-    l_min = eig_val[0]
-    l_max = eig_val[-1]
-
-    kappa = l_max/l_min
-    precision = 10**-13
-
-    t = int(kappa*precision)
-    print(t)
-
-
-
-
-    w_initial = np.ones((10,1))
-    w = w_initial
-
-
-
-######################################################
-# Ridge Regression Implementation
-######################################################
-
-if ridgeregression:
-
-    print(x_train.shape)
-
-
-    l = 0.1
-
-    Id = np.identity(x_train.shape[1])
-
-    variance = x_train.T.dot(x_train) + l*Id
-    var_inv = np.linalg.inv(variance)
-
-    covariance = x_train.T.dot(y_train)
-
-    weights = var_inv.dot(covariance)
-
-
-
-
-
-
-    print(weights)
-
-    # predict validate data
-    print("calculating predictions...")
-    prediction = x_validate.dot(weights)
-    prediction = prediction.transpose()
-
-    print(y_validate.shape)
-    y_validate = np.reshape(y_validate,(5000,1))
-    prediction = np.reshape(prediction,(5000,1))
-
-    # rmse
-    print("calculate RMSE..")
-    RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-    print("RMSE:{0:0.15f}".format(RMSE))
-
-
-
-######################################################
-# SKLEARN Implementation
-######################################################
-
-reg = linear_model.Ridge(alpha=0.0000005)
-reg.fit(x_train,y_train)
-
-prediction = reg.predict(x_validate)
-
-y_validate = np.reshape(y_validate,(5000,1))
-prediction = np.reshape(prediction,(5000,1))
-
-
-print("calculate RMSE..")
-RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-print("RMSE:{0:0.15f}".format(RMSE))
-
-
-
-reg = linear_model.LinearRegression(fit_intercept=True)
-reg.fit(x_train,y_train)
-
-prediction = reg.predict(x_validate)
-
-y_validate = np.reshape(y_validate,(5000,1))
-prediction = np.reshape(prediction,(5000,1))
-
-
-print("calculate RMSE..")
-RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-print("RMSE:{0:0.15f}".format(RMSE))
-
-reg = linear_model.Lasso(alpha=0.001)
-reg.fit(x_train,y_train)
-
-prediction = reg.predict(x_validate)
-
-_validate = np.reshape(y_validate,(5000,1))
-prediction = np.reshape(prediction,(5000,1))
-
-
-print("calculate RMSE..")
-RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-print("RMSE:{0:0.15f}".format(RMSE))
-
-
-reg = linear_model.ElasticNet(alpha=0.00001)
-reg.fit(x_train,y_train)
-
-prediction = reg.predict(x_validate)
-
-_validate = np.reshape(y_validate,(5000,1))
-prediction = np.reshape(prediction,(5000,1))
-
-
-print("calculate RMSE..")
-RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-print("RMSE:{0:0.15f}".format(RMSE))
-
-reg = linear_model.Lars(fit_intercept=True)
-reg.fit(x_train,y_train)
-
-prediction = reg.predict(x_validate)
-
-_validate = np.reshape(y_validate,(5000,1))
-prediction = np.reshape(prediction,(5000,1))
-
-
-print("calculate RMSE..")
-RMSE = mean_squared_error(y_validate, prediction) ** 0.5
-print("RMSE:{0:0.15f}".format(RMSE))
-
-
-pred_test = reg.predict(x_test)
-
-######################################################
-# Result Output
-######################################################
-
-#predict test data and store into result.csv
-#pred_test = x_test.dot(weights)
-pred_test = pred_test.transpose()
-pred_test = pred_test.reshape((-1,1))
-test_data = test_data[1:2001,0]
-test_data = test_data.reshape((-1,1))
-result = np.hstack((test_data,pred_test))
-np.savetxt("result.csv", result, delimiter=",")
-
+outputFrame = pandas.DataFrame(data=output)
+outputFrame.to_csv('submission.csv',header=['Id','y'],index=False)
 
