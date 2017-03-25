5f31efca30865af4d587f088d66c40c985c0dcaf
weders <weders@student.ethz.ch>
2017-03-25 08:50:43 +0100

    linear least squares (incl. higher order)

--- task1/main.py
+++ task1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas
+from sklearn.metrics import mean_squared_error
 
 
 ##########################################
@@ -18,3 +19,79 @@ test_data = test_data.as_matrix()
 
 y_training = training_data[:,1]
 x_training = training_data[:,2:]
+
+test_data_Id = test_data[:,0]
+test_data_x = test_data[:,1:]
+
+test_data_Id = test_data_Id.astype(int)
+test_data_Id = test_data_Id.T
+
+
+##########################################
+# Split Training Data
+##########################################
+
+fraction = 2 # enter fraction number for training data split
+
+SIZE_OF_TRAINING_SET = int(training_data.shape[0]/fraction)
+
+x_train = x_training[:SIZE_OF_TRAINING_SET,:]
+y_train = y_training[:SIZE_OF_TRAINING_SET]
+
+x_validation = x_training[SIZE_OF_TRAINING_SET:,:]
+y_validation = y_training[SIZE_OF_TRAINING_SET:]
+
+
+##########################################
+# Routine Definitions
+##########################################
+
+def error_calculation(y_pred,y):
+    RMSE = mean_squared_error(y,y_pred) ** 0.5
+    print(RMSE)
+    return
+
+
+
+
+##########################################
+# Preferences
+##########################################
+
+LLS = False # Linear Least Squares (order 1)
+LLS_order = True # Linear Least Squares (order 2)
+order = 2 # order of linear least squares
+
+
+
+
+##########################################
+# Linear Least Squares
+##########################################
+
+if LLS:
+    # Training
+    covariance = x_train.T.dot(x_train) # covariance matrix for linear least squares computation
+    weights = np.linalg.inv(covariance).dot(x_train.T.dot(y_train)) # weights of linear least squares
+
+    y_pred = x_validation.dot(weights) # prediction for validation set
+    error_calculation(y_pred, y_validation) # error calculation for validation
+
+if LLS_order:
+
+    i = 2
+    while i <= order:
+        x_train = np.hstack((x_train,x_train**i))
+        x_validation = np.hstack((x_validation,x_validation**i))
+        i += 1
+
+
+    covariance = x_train.T.dot(x_train) # covariance matrix for linear least squares computation
+    weights = np.linalg.inv(covariance).dot(x_train.T.dot(y_train)) # weights of linear least squares
+
+
+    y_pred = x_validation.dot(weights) # prediction for validation set
+    error_calculation(y_pred, y_validation) # error calculation for validation
+
+
+
